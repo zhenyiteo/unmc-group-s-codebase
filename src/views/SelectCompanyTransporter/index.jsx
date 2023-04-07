9c2059d79c3750d6f51041a9a02c59feba81928d
Frds232 <fakhirfirdaus7@gmail.com>
2023-04-08 00:54:56 +0800

    latest button design

--- src/views/SelectCompanyTransporter/index.jsx
+++ src/views/SelectCompanyTransporter/index.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './index.module.css';
+import { Button } from 'antd';
 
 export default function SelectCompany() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
-  const handleCompanyClick = (company) => {
-    sessionStorage.setItem('accountName', company);
-    navigate(`/transporter/availableJob`);
-  }
+  
 
   return (
-    <div className={styles.container}>
-      <h1>Select a Company</h1>
-      <ul className={styles.companyList}>
-        <li>
-          <button onClick={() => handleCompanyClick("J&T Express")}>J&T Express</button>
-        </li>
-        <li>
-          <button onClick={() => handleCompanyClick("DHL Express")}>DHL Express</button>
-        </li>
-        <li>
-          <button onClick={() => handleCompanyClick("Ninjavan")}>Ninjavan</button>
-        </li>
-      </ul>
-    </div>
+    
+
+    <div style={{ display: "flex", justifyContent: "center", padding: 150, alignItems: "center" }}>
+
+    <div style={{ marginRight: 70 }}>
+      <span style={{ fontSize: 30, fontWeight: "bold", marginBottom: 50, marginLeft: 120}}>Select a Company</span>
+      <div style={{ display: "flex", flexDirection: "column", marginTop: 30 }}>
+        <Button type='primary' style={{ marginTop: 10, width: 500, height: 43, borderRadius: 10 }} onClick={() => {
+          navigate("/transporter/availableJob");
+        }}>J&T Express</Button>
+        <Button type='primary' style={{ marginTop: 10, width: 500, height: 43, borderRadius: 10 }} onClick={() => {
+          navigate("/transporter/availableJob");
+        }}>DHL Express</Button>
+        <Button type='primary' style={{ marginTop: 10, width: 500, height: 43, borderRadius: 10 }} onClick={() => {
+          navigate("/transporter/availableJob");
+        }}>Ninjavan</Button>
+      </div>
+      </div>
+      </div>
+
+    
   );
 }
